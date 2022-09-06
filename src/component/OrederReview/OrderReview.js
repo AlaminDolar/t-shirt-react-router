4e9f2f723714aeb9fb88311a4b0951f79ab573e6
AlaminDolar <amin15-9898@diu.edu.bd>
2022-09-06 16:43:07 +0600

    basic structure setup

---
+++ src/component/OrederReview/OrderReview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>
+                This is your Order Review
+            </h3>
+        </div>
+    );
+};
+
+export default OrderReview;
